5ded8910dece92083c69fbf8d234d04171e1cdc8
9xbt <flippy632@gmail.com>
2025-12-14 18:29:53 +0100

    gcc: support shared

--- gcc/config/i386/bentobox.h
+++ gcc/config/i386/bentobox.h
@@ -7,16 +7,22 @@
 #undef LIB_SPEC
 #define LIB_SPEC "-lc" /* link against C standard library */
 
-
-
 /* Files that are linked before user code.
    The %s tells GCC to look for these files in the library directory. */
 #undef STARTFILE_SPEC
-#define STARTFILE_SPEC "crt1.o%s crti.o%s crtbegin.o%s"
+#define STARTFILE_SPEC "%{!shared: %{static-pie|pie|!no-pie:Scrt1.o%s;:crt1.o%s}} crti.o%s %{shared|static-pie|pie|!no-pie:crtbeginS.o%s;:crtbegin.o%s}"
 
 /* Files that are linked after user code. */
 #undef ENDFILE_SPEC
-#define ENDFILE_SPEC "crtend.o%s crtn.o%s"
+#define ENDFILE_SPEC "%{shared|static-pie|pie|!no-pie:crtendS.o%s;:crtend.o%s} crtn.o%s"
+
+#define GNU_USER_LINK_EMULATION32 "elf_i386"
+#define GNU_USER_LINK_EMULATION64 "elf_x86_64"
+#define GNU_USER_LINK_EMULATIONX32 "elf32_x86_64"
+
+#define GNU_USER_DYNAMIC_LINKER32 "/does_not_exist"
+#define GNU_USER_DYNAMIC_LINKER64 "/usr/lib/ld.so"
+#define GNU_USER_DYNAMIC_LINKERX32 "/does_not_exist"
 
 /* Additional predefined macros. */
 #undef TARGET_OS_CPP_BUILTINS
