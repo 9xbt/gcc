1451a62f10cc362d9c89250d809b2dd1fedbddb1
9xbt <flippy632@gmail.com>
2026-01-03 15:30:27 +0100

    bentobox: aarch64 support

---
+++ gcc/config/aarch64/bentobox.h
@@ -0,0 +1,37 @@
+/* Useful if you wish to make target-specific GCC changes. */
+#undef TARGET_BENTOBOX
+#define TARGET_BENTOBOX 1
+
+/* Default arguments you want when running your
+   i686-myos-gcc/x86_64-myos-gcc toolchain */
+#undef LIB_SPEC
+#define LIB_SPEC "-lc" /* link against C standard library */
+
+/* Files that are linked before user code.
+   The %s tells GCC to look for these files in the library directory. */
+#undef STARTFILE_SPEC
+#define STARTFILE_SPEC "%{!shared: %{static-pie|pie|!no-pie:Scrt1.o%s;:crt1.o%s}} crti.o%s %{shared|static-pie|pie|!no-pie:crtbeginS.o%s;:crtbegin.o%s}"
+
+/* Files that are linked after user code. */
+#undef ENDFILE_SPEC
+#define ENDFILE_SPEC "%{shared|static-pie|pie|!no-pie:crtendS.o%s;:crtend.o%s} crtn.o%s"
+
+#undef LINK_SPEC
+#define LINK_SPEC "%{h*} %{version:-v} \
+   %{static:-Bstatic} %{shared:-shared} %{symbolic:-Bsymbolic} \
+   %{!static:%{!static-pie:--eh-frame-hdr}} \
+   %{!static:%{!static-pie:%{!shared:%{!r:-dynamic-linker " GNU_USER_DYNAMIC_LINKER "}}}} \
+   -m aarch64linux"
+
+#define GNU_USER_DYNAMIC_LINKER "/usr/lib/ld.so"
+
+/* Additional predefined macros. */
+#undef TARGET_OS_CPP_BUILTINS
+#define TARGET_OS_CPP_BUILTINS()      \
+  do {                                \
+    builtin_define ("__bentobox__");      \
+    builtin_define ("__unix__");      \
+    builtin_assert ("system=bentobox");   \
+    builtin_assert ("system=unix");   \
+    builtin_assert ("system=posix");   \
+  } while(0);
